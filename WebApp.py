3f1178a6ebf059b894d07909fda4a08fb539bc85
Garvit Chouhan <68856476+Garvit9000c@users.noreply.github.com>
2021-03-25 21:58:49 +0530

    Create WebApp.py

---
+++ WebApp.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import streamlit as st
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+def import_and_predict(image_data, model):
+        size = (100,100)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img_resize = (cv2.resize(img, dsize=(75, 75),interpolation=cv2.INTER_CUBIC))/255.
+        img_reshape = img_resize[np.newaxis,...]
+        prediction = model.predict(img_reshape)
+        return prediction
+
+model = tf.keras.models.load_model('/content/drive/MyDrive/Data_set/Tumor_CNN.hdf5')
+
+st.write("""
+         # Early Stage Brain Tumor Detection Using Convoluted Neural Network
+         """
+         )
+file = st.file_uploader("Please upload an image file", type=["jpg"])
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    if np.argmax(prediction) == 0:
+        st.write("The Brain is Normal!")
+    else:
+        st.write("Tumor Detected!")
+    st.text("Probability (0: Non-Tumorous, 1: Tumorous")
+    st.write(prediction)
